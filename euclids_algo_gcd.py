375f1db3c2f991037ef34364a4e4f5ec587e692b
tankasalamanisha <tankasalamanisha95@gmail.com>
2022-06-14 09:23:08 +0530

    Euclids algorithm for gcd version 1

---
+++ euclids_algo_gcd.py
@@ -0,0 +1,21 @@
+import sys
+
+def gcd(m:int , n:int)->int:
+    """
+    Function definition to compute greatest common divisor of 2 numbers.
+    Parameters:
+    -------------
+    m, n -> input from user; type: int.
+    Returns:
+    -------------
+    mrcf -> most recent greates common divisor of m and n"""
+   #Assume  m>=n
+    if  m<n : 
+        (m,n) = (n,m)  #  simultaneous assignment
+    if (m%n) ==0:
+        return n
+    diff = m-n  #diff >n ? Possible
+    return gcd(max(n,diff), min(n,diff))
+
+if __name__ == "__main__":
+    print(f"Greatest common divisor of {sys.argv[1]} and {sys.argv[2]} is: {gcd(int(sys.argv[1]), int(sys.argv[2]))}")
