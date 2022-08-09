365b765debfc9a2e6613ba5c200760ed69865d85
Manisha Akshita T V <tankasalamanisha95@gmail.com>
2022-08-09 16:14:52 +0530

    simple search

--- search.py
+++ search.py
@@ -1,6 +1,6 @@
 import sys
 
-def binarysearch(seq:list, v:int)->bool:
+def search(seq:list, v:int)->bool:
     """Function to search an element in an unsorted list/array.
     Params:
     ---------
@@ -15,7 +15,7 @@ def binarysearch(seq:list, v:int)->bool:
             return True
     return False
 if __name__ == "__main__":
-    if binarysearch(seq = list(map(int,sys.argv[1])),v = int(sys.argv[2])):
+    if search(seq = list(map(int,sys.argv[1])),v = int(sys.argv[2])):
         print(f"The value {sys.argv[2]} is found in the list")
     else:
         print("Not found")
