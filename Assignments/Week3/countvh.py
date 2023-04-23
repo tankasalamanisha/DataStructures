ee513a32304f3c4844cb898b42590e01c28e5a1d
Manisha Akshita T V <72745106+tankasalamanisha@users.noreply.github.com>
2023-04-23 13:25:00 +0000

    Completed valley hill implimentation

---
+++ Assignments/Week3/countvh.py
@@ -0,0 +1,21 @@
+import sys
+
+def countvh(l:list)->list:
+    """Function to return valleys and hills in a list
+    Args:
+        l(list): input list containing numbers
+
+    Returns:
+        list: list of valleys and hill elements.
+    """
+    vh_list=[]
+    for i in range(1,len(l)-2):
+        if l[i]> l[i-1] and l[i]> l[i+1]:
+            vh_list.append(l[i])
+        elif l[i]<l[i-1] and l[i]<l[i+1]:
+            vh_list.append(l[i])
+    return vh_list
+
+if __name__ == "__main__":
+    my_list=sys.argv[1].split('[')[1].split(']')[0].split(',')
+    print(countvh(list(map(int,my_list))))
