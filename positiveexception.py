fe6470f0fd6afff020639057161625c577fd0fe9
Manisha Akshita T V <72745106+tankasalamanisha@users.noreply.github.com>
2023-05-25 12:23:02 +0000

    Completed Exception Handling implimentation

---
+++ positiveexception.py
@@ -0,0 +1,13 @@
+def append_new_dict_element(my_dict,k_,v_):
+    try:
+        my_dict[k_].append(v_)
+    except KeyError as ke:
+        print(f'{ke}: Key not in dict... Adding a new key entry...')
+        my_dict[k_] = [v_]
+    
+    return my_dict
+
+if __name__ == "__main__":
+    new_dict = {"manisha":[20,30], "mohan":[50,60]}
+    print(append_new_dict_element(new_dict, "Anand",50))
+
